f701f5e90d20d5e8fe5ca0ebd17bfca33d97db73
Ghdhdhhake <haker159@163.com>
2024-09-03 22:57:56 +0800

    the code have erro

--- cn003/cn003.c
+++ cn003/cn003.c
@@ -1,5 +1,20 @@
+//有问题的代码
 #include<stdio.h>
-void main()
+int main()
 {
-	printf("hello world!");
+    int i, j, n, x;
+    for (i = 1; i < 85; i++)
+    {
+        if ((i % 168) == 0)
+        {
+            j = 168 / i;
+            if (i > j && ((i + j) % 2) == 0 && ((i - j) % 2) == 0)
+            {
+                n = (i - j) / 2;
+                x = n * n - 100;
+                printf("The number is %d\n", x);
+            }
+        }
+    }
+    return 0;
 }
