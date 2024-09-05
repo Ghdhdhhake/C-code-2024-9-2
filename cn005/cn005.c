9baab0de558affc08c26a790b5ec49d28e497b1f
Ghdhdhhake <haker159@163.com>
2024-09-05 23:52:22 +0800

    compare numbers

--- cn005/cn005.c
+++ cn005/cn005.c
@@ -1,25 +1,16 @@
 #include<stdio.h>
 #define swap(a,b) { a = a + b; b = a - b; a = a - b;}
+
 void main()
 {
 	printf("Please enter three number:");
-	int number[3];
-	for (int i = 0; i < 3; i++)
-	{
-		scanf_s("%d", &number[i]);
-	}
-	for (int i = 1; i <= 2; i++)
-	{
-		for (int j = 0; j <= 2 - i; j++)
-		{
-			if (number[j] > number[j + 1])
-			{
-				swap(number[j], number[j + 1]);
-			}
-		}
-	}
-	for (int i = 0; i < 3; i++)
-	{
-		printf("%d ", number[i]);
-	}
+	int x, y, z;
+	scanf_s("%d %d %d", &x, &y, &z);
+	if (x > y)
+		swap(x, y);
+	if (x > z)
+		swap(x, z);
+	if (y > z)
+		swap(y, z);
+	printf("%d < %d < %d", x, y, z);
 }
