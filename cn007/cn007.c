e74af94cf513342ba39e0b41b68b3efe77d66adc
Ghdhdhhake <haker159@163.com>
2024-09-06 23:41:56 +0800

    out char

---
+++ cn007/cn007.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+	char a = 126, b = 36;
+	printf("%c%c%c%c%c\n", b, a, a, a, b);
+	printf("%c%c%c%c%c\n", a, b, a, b, a);
+	printf("%c%c%c%c%c\n", a, a, b, a, a);
+	printf("%c%c%c%c%c\n", a, b, a, b, a);
+	printf("%c%c%c%c%c\n", b, a, a, a, b);
+}
